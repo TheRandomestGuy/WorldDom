db362c514bd0a3eab8c2d4f41d3cf961e303ebc8
TheRandomestGuy <arjun.mehan1@gmail.com>
2020-02-19 17:26:52 -0500

    added help

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'Njc5MTQwNzgxODY4NDQ5ODE0.XktBdg.hMMb9Vl8NPwtjVDoBjloqE7StxM';
+const token = 'Njc5MTQwNzgxODY4NDQ5ODE0.Xk2yOA.7S7R7pfHRB_zV38a9Rt1889ARn0';
 const Pre = '$';
 
 bot.on('ready', () => {
-    console.log('Im online, sir. do you copy, sir');
+    console.log('Im online, sir. do you copy, sir.');
 })
 
 bot.on('message', msg =>{
     let args = msg.content.substring(Pre.length).split(" ");
     switch(args[0]){
         case 'help':
-            msg.channel.send()
+            help = new Discord.RichEmbed()
+            .setColor(0xff0000)
+            .addField('I dont do anything yet but...', `I will be a game where you will have to build an army and conquer the world for your empire`)
+            msg.channel.send(help)
         break;
     }
 })
